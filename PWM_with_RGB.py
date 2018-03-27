078b602cf55220fbfa9f74d444736d7ba0831cf4
joshhyde13 <joshhyde13@gmail.com>
2018-03-26 20:57:32 -0600

    clean-up from original development

--- PWM_with_RGB.py
+++ PWM_with_RGB.py
@@ -1,41 +1,61 @@
+"""
+Used starter code from:
+http://raspi.tv/2013/rpi-gpio-0-5-2a-now-has-software-pwm-how-to-use-it
+"""
 
-import RPi.GPIO as GPIO # always needed with RPi.GPIO
+import RPi.GPIO as GPIO 
 import time
-
-GPIO.cleanup()
-  
-GPIO.setmode(GPIO.BOARD)  # choose BCM or BOARD numbering schemes.
-
-def PWM_setup(pins = {'red': 11, 'green':12, 'blue':13}):
+ 
+def PWM_setup(pins={'red':11, 'green':12, 'blue':13}, Hz={'red':50, 'green':50, 'blue':50}):
     """
     Set GPIO pins as output
+    INPUT:
+       dictionaries: pins = {'pin_name': pin_numer} --> {string: int}
+                     hertz = {'pin_name': hertz} --> {string: int}
     """
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BOARD)  # choose BCM or BOARD numbering schemes.
     for i in pins:
         GPIO.setup(pins[i],GPIO.OUT)
+    #set object as global
+    global p_red, p_green, p_blue
+    # create an object for PWM on ports per dictionary at 50 Hertz   
+    p_red = GPIO.PWM(pins['red'], Hz['red'])
+    p_green = GPIO.PWM(pins['green'],Hz['green'])
+    p_blue = GPIO.PWM(pins['blue'], Hz['blue'])
 
-PWM_setup()  
-  
-# create an object for PWM on ports 11, 12, 13 at 50 Hertz   
-p_red = GPIO.PWM(11, 100)
-p_green = GPIO.PWM(12, 50)
-p_blue = GPIO.PWM(13, 100)
- 
 
-p_red.start(0)             # start the PWM on x percent duty cycle
-p_blue.start(0)
-p_green.start(0)
-for i in range(1,50):
-    p_red.ChangeDutyCycle(i)
-    p_blue.ChangeDutyCycle(50-i)
-    p_green.ChangeDutyCycle(25-i/2)
-    time.sleep(.2)
+def run(t=.2, duty_cycle_range=50):
+    """
+    INPUT:
+       t = time in seconds between PWM changes
+       duty_cycle_range = range from 0 to 100
+    """
+    # start the PWM on 0 percent duty cycle
+    p_red.start(0)
+    p_blue.start(0)
+    p_green.start(0)
+    # duty cycle value can be 0.0 to 100.0%, floats are OK
+    for i in range(1,duty_cycle_range):
+        p_red.ChangeDutyCycle(i)
+        p_blue.ChangeDutyCycle(50-i)
+        p_green.ChangeDutyCycle(25-i/2)
+        time.sleep(t)
+    # can also change the frequency Hz (floats also work) --> p_red.ChangeFrequency(100)
 
-# duty cycle value can be 0.0 to 100.0%, floats are OK  
-#p_red.ChangeFrequency(100)  # change the frequency to 100 Hz (floats also work)            
+def stop():
+    """
+    Stops the PWM output
+    """
+    p_red.stop()
+    p_blue.stop()
+    p_green.stop()
 
-p_red.stop()                # stop the PWM output  
-p_blue.stop()
-p_green.stop()
+def main():
+    PWM_setup()
+    run()
+    stop()
+    GPIO.cleanup()
 
-GPIO.cleanup()          # when your program exits, tidy up after yourself
+if __name__=="__main__":
+    
+    main()
