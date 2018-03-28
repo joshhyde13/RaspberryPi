736082c1abf433be1c30f704db8443c31133d4b9
joshhyde13 <joshhyde13@gmail.com>
2018-03-27 21:20:42 -0600

    updated with comments

--- PWM_input.py
+++ PWM_input.py
@@ -31,11 +31,58 @@ def inputs():
 
 def run():
     inputs()
-    p_red.ChangeDutyCycle(red_duty)
-    p_blue.ChangeDutyCycle(blue_duty)
-    p_green.ChangeDutyCycle(green_duty)
-    time.sleep(t)
+    p_red.ChangeDutyCycle(int(red_duty))
+    p_blue.ChangeDutyCycle(int(blue_duty))
+    p_green.ChangeDutyCycle(int(green_duty))
+    print_values()
+    time.sleep(int(t))
+
+
+def print_statement():
+    print("\n============================")
+    print("This progam allows you to use")
+    print("pulse width modulation (PWM)")
+    print("to create any color using a")
+    print("red, green, blue LED light.\n")
+    print("Thousands of tiny red, green and")
+    print("blue lights are how modern TVs")
+    print("project images on the screen.\n")
+    print("The color is determined by the")
+    print("ratio of the three lights. The ")
+    print("brightness is determined by the.")
+    print("total value of the duty cycles...")
+    print("which is simply the % of time the")
+    print("light is pulsed off.\n")
+    """
+    print("")
+    print("         brigher      dimmer")
+    print("         ___^____  ______^______")
+    print("        /        \\/             \\")
+    print("ON-->   ___   ___       ___      _")
+    print("       |   | |   |     |   |    |")
+    print("       |   | |   |     |   |    |")
+    print("       |   | |   |     |   |    |")
+    print("       |   | |   |     |   |    |")
+    print("       |   |_|   |_____|   |____|")
+    print("OFF-->")
+    print("===============================\n")
+    """
+    print("Press enter to begin.")
+    print("Press ctrl + c to end.")
+    input()
     
+def print_values():
+    total = int(red_duty) + int(blue_duty) + int(green_duty)
+    red_pct = int(red_duty)/total*100
+    blue_pct = int(blue_duty)/total*100
+    green_pct = int(green_duty)/total*100
+    tot_pct = red_pct + blue_pct + green_pct
+    print("\nRED   BLUE    GREEN   Total")
+    print(" {}    {}      {}      {}".format(red_duty, blue_duty, green_duty, total))
+    print(" {}%   {}%     {}%     {}%".format(round(red_pct), round(blue_pct), round(green_pct), round(tot_pct)))
+    print("\nPress enter to continue,")
+    print("ctrl + c to end....")
+        
 def stop():
     """
     Stops the PWM output
@@ -45,11 +92,16 @@ def stop():
     p_green.stop()
 
 def main():
-    PWM_setup()
-    run()
-    stop()
+    print_statement()
+    for i in range(100):
+        PWM_setup()
+        run()
+        stop()
+        input()
     GPIO.cleanup()
 
 if __name__=="__main__":
-    
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
