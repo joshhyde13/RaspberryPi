cbe9251abb29e60a3e303694c40d1214ef9f3b0d
joshhyde13 <joshhyde13@gmail.com>
2018-03-27 18:35:54 -0600

    Updated program to run more colors and with comments for program

--- PWM_with_RGB.py
+++ PWM_with_RGB.py
@@ -5,8 +5,27 @@ http://raspi.tv/2013/rpi-gpio-0-5-2a-now-has-software-pwm-how-to-use-it
 
 import RPi.GPIO as GPIO 
 import time
+
+def print_statement():
+    print("\n=================================")
+    print("This program will show the many")
+    print("colors that can be created with")
+    print("a red, green, blue LED light.")
+    print("=================================\n")
+    print("Thousands of tiny red, green and")
+    print("blue lights are how modern TVs")
+    print("project images on the screen.\n")
+    #print("This program is set at 100Hz with")
+    #print("with duty cycles combined as")
+    #print("multiples of 10.\n")
+    #print("For hardware seup see: link")
+    print("To begin, please press enter.")
+    print("To cancel press ctrl + C\n")
+    print("ENJOY!!!\n")
+    input()
+
  
-def PWM_setup(pins={'red':11, 'green':12, 'blue':13}, Hz={'red':50, 'green':50, 'blue':50}):
+def PWM_setup(pins={'red':11, 'green':12, 'blue':13}, Hz={'red':500, 'green':500, 'blue':500}):
     """
     Set GPIO pins as output
     INPUT:
@@ -24,7 +43,7 @@ def PWM_setup(pins={'red':11, 'green':12, 'blue':13}, Hz={'red':50, 'green':50, 
     p_blue = GPIO.PWM(pins['blue'], Hz['blue'])
 
 
-def run(t=.2, duty_cycle_range=50):
+def run(t=.1):
     """
     INPUT:
        t = time in seconds between PWM changes
@@ -35,11 +54,15 @@ def run(t=.2, duty_cycle_range=50):
     p_blue.start(0)
     p_green.start(0)
     # duty cycle value can be 0.0 to 100.0%, floats are OK
-    for i in range(1,duty_cycle_range):
-        p_red.ChangeDutyCycle(i)
-        p_blue.ChangeDutyCycle(50-i)
-        p_green.ChangeDutyCycle(25-i/2)
-        time.sleep(t)
+    l = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    for r in l:
+        for b in l:
+            for g in l:
+                p_red.ChangeDutyCycle(r)
+                p_blue.ChangeDutyCycle(b)
+                p_green.ChangeDutyCycle(g)
+                time.sleep(t)
+                print("r:{} b:{} g:{}".format(r, b, g))
     # can also change the frequency Hz (floats also work) --> p_red.ChangeFrequency(100)
 
 def stop():
@@ -49,13 +72,18 @@ def stop():
     p_red.stop()
     p_blue.stop()
     p_green.stop()
+    print("\nResetting pins...")
 
 def main():
     PWM_setup()
+    print_statement()
     run()
     stop()
     GPIO.cleanup()
+    print("Done")
 
 if __name__=="__main__":
-    
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        stop()
